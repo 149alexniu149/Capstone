aac114561e0d85e745cf587a7ea41a89f44a6860
149alexniu149 <alexniu149@gmail.com>
2015-03-14 22:14:21 -0400

    Started front end development

---
+++ frontend/frontendJS.js
@@ -0,0 +1,16 @@
+function bitfrequest()
+{
+	//define variables for use in DB query
+	var vol = document.getElementById("bitfVol").value;
+	var bid = document.getElementById("bitfBid").value;
+	var ask = document.getElementById("bitfAsk").value;
+}
+
+function bitfrequest()
+{
+	//define variables for use in DB query
+	var vol = document.getElementById("bitsVol").value;
+	var bid = document.getElementById("bitsBid").value;
+	var ask = document.getElementById("bitsAsk").value;
+}
+	
