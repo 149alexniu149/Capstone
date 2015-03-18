5682e0ba9035e3d1c2147ddaf78608307b47f20d
149alexniu149 <alexniu149@gmail.com>
2015-03-17 21:27:33 -0400

    Added space for adding in high charts

--- frontend/frontendJS.js
+++ frontend/frontendJS.js
@@ -1,16 +1,17 @@
 function bitfrequest()
 {
 	//define variables for use in DB query
-	var vol = document.getElementById("bitfVol").value;
+	/*var vol = document.getElementById("bitfVol").value;
 	var bid = document.getElementById("bitfBid").value;
-	var ask = document.getElementById("bitfAsk").value;
+	var ask = document.getElementById("bitfAsk").value;*/
+	alert("ran")
 }
 
 function bitfrequest()
 {
 	//define variables for use in DB query
-	var vol = document.getElementById("bitsVol").value;
+	/*var vol = document.getElementById("bitsVol").value;
 	var bid = document.getElementById("bitsBid").value;
-	var ask = document.getElementById("bitsAsk").value;
+	var ask = document.getElementById("bitsAsk").value;*/
 }
 	
